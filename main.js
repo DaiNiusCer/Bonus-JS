10d294de2e76d567c2ef11bc157c4f30e10c4596
DaiNiusCer <d.cerkauskas@gmail.com>
2022-03-24 17:00:57 +0200

    Pridėtas trynimas ir papildoma forma

--- main.js
+++ main.js
@@ -12,11 +12,39 @@ fetch("http://localhost:3000/motoData")
     <h2>${moto.title}</h2>
     <img src="${moto.image}">
     <p>${moto.paragraph}</p>
+    <button class="trintiMoto">Delete</button>
     </div>
     `
       document.querySelector("#motoIsvedimas").innerHTML = output;
     })
 
+    document.querySelectorAll(".trintiMoto").forEach(btn => {
+      btn.addEventListener("click", e => {
+        let motoId = e.target.parentElement.id
+
+
+        naikintiMoto(motoId)
+      })
+    })
+
+
   })
 
 
+
+
+
+
+
+
+
+
+
+
+//Elemento panaikinimas
+
+let naikintiMoto = (id) => {
+  fetch(`http://localhost:3000/motoData${id}`, {
+    method: "DELETE"
+  })
+}
