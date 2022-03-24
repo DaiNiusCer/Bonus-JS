dd546926c2f3ea3f163d5950cf5c6f0298231c53
DaiNiusCer <d.cerkauskas@gmail.com>
2022-03-24 17:56:12 +0200

    Sutavarkytas elemento naikinimas

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ fetch("http://localhost:3000/motoData")
 //Elemento panaikinimas
 
 let naikintiMoto = (id) => {
-  fetch(`http://localhost:3000/motoData${id}`, {
+  fetch(`http://localhost:3000/motoData/${id}`, {
     method: "DELETE"
   })
 }
@@ -52,27 +52,5 @@ let naikintiMoto = (id) => {
 
 //Elemento pridėjimas
 
-document.querySelector("#pridetiMotocikla").addEventListener("submit", e => {
-  e.preventDefault()
-  console.log(e)
-  let motoVardas = e.target.elements.pavadinimas.value
-  console.log(motoVardas)
-
-  let aprasas = e.target.elements.paragrafas.value
-  console.log(aprasas)
-
-  let nuotrauka = e.target.elements.nuoroda.value
-
-  fetch("http://localhost:3000/motoData"), {
-    method: "POST",
-    headers: {
-      'Accept': 'application/json, text/plain,*/*',
-      'Content-Type': 'application/json'
-    },
-
-    body: JSON.stringify({ title: motoVardas, paragraph: aprasas, image: nuotrauka })
-  }
-
-})
 
 //Elemento pridėjimas pabaiga
